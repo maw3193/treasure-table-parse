d25dbbaf9daf854e94d01fe61008c659edc0a125
Jonathan Maw <jonathan.maw@gmail.com>
2016-05-16 22:36:23 +0100

    Add md5sum checking of the PDF
    If the input file is different, there may be unexpected changes, and
    making it flexible enough would be a lot of work

--- parse.sh
+++ parse.sh
@@ -5,8 +5,15 @@ if test -z "$1"; then
 fi
 
 INPUT_PDF="$1"
-PAGE_COUNT="$(pdfinfo "$1" | grep "^Pages:" | sed 's/Pages: \+//')"
+EXPECTED_MD5SUM="fafe4527b594751ca03777cadcce83ef"
 
+FOUND_MD5SUM=$(md5sum "$INPUT_PDF" | cut -d' ' -f1)
+
+if ! "$EXPECTED_MD5SUM" = "$FOUND_MD5SUM"; then
+	echo "WARNING: md5sum doesn't match what was expected. parsing may not work!"
+fi
+
+PAGE_COUNT="$(pdfinfo "$1" | grep "^Pages:" | sed 's/Pages: \+//')"
 
 # Convert the PDF into text files of individual pages
 mkdir -p pages
